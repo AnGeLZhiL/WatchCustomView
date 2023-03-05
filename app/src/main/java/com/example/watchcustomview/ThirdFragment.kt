63a9236b7faf03ea902ed36e90be3d30a365e4e8
AnGeLZhiL <angelina.zhilyakova.2002@mail.ru>
2023-03-05 13:10:01 +0300

    Добавление навигации и элементов на фрагменты

---
+++ app/src/main/java/com/example/watchcustomview/ThirdFragment.kt
@@ -0,0 +1,24 @@
+package com.example.watchcustomview
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.watchcustomview.databinding.FragmentThirdBinding
+
+class ThirdFragment : Fragment() {
+    private lateinit var binding: FragmentThirdBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentThirdBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
